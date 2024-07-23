e6480f0df5011dc13e15e0599fd588c2252aec25
Rodrimansidub14 <man22611@uvg.edu.gt>
2024-07-23 11:21:59 -0600

    implementación 2do poligono

--- lab1-FAP/src/main.rs
+++ lab1-FAP/src/main.rs
@@ -21,7 +21,28 @@ fn main() {
     framebuffer.draw_line(100, 100, 700, 500);
     framebuffer.draw_line(700, 100, 100, 500);
 
+<<<<<<< Updated upstream
     // Save the framebuffer as a BMP file
+=======
+
+    // Dibujar el primer polígono con borde blanco más grueso
+    draw_polygon(&mut framebuffer, &polygon1, 0xFFFFFF, 2); // Orilla blanca con grosor 2
+
+    // Rellenar el primer polígono con color amarillo
+    fill_polygon(&mut framebuffer, &polygon1, 0xFFFF00); // Relleno amarillo
+
+    let polygon2 = vec![
+        (521, 235), (488, 186), (539, 151), (574, 202),
+    ];
+
+    // Dibujar el segundo polígono con borde blanco más grueso
+    draw_polygon(&mut framebuffer, &polygon2, 0xFFFFFF, 2); // Orilla blanca con grosor 2
+
+    // Rellenar el segundo polígono con color azul
+    fill_polygon(&mut framebuffer, &polygon2, 0x0000FF); // Relleno azul
+
+    // Guardar el framebuffer en un archivo BMP
+>>>>>>> Stashed changes
     framebuffer.render_buffer("output.bmp").expect("Failed to save BMP file");
 
     println!("Framebuffer rendered to output.bmp");
